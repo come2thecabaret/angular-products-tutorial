4d48f58ceb65cddc01f978ffeedc82a1ca421e54
Joey Conroy <come2thecabaret@gmail.com>
2017-11-04 13:43:10 -0400

    http service added

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -14,6 +14,7 @@ export class ProductListComponent implements OnInit {
     imageMargin: number =2;
     showImage: boolean = false;
     _listFilter: string;
+    errorMessage: string;
     get listFilter(): string {
         return this._listFilter;
     }
@@ -38,8 +39,12 @@ export class ProductListComponent implements OnInit {
         this.showImage = !this.showImage;
     }
     ngOnInit(): void {
-        this.products = this._productService.getProducts();
-        this.filteredProducts = this.products;        
+        this._productService.getProducts()
+            .subscribe(products => {
+                this.products = products;
+                this.filteredProducts = this.products;                        
+            },
+                error => this.errorMessage = <any>error);
     }
     onNotify(message: string): void {};
 }
